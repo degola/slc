a16321eb2e9d8f786538f152f1d77ceee7043cbf
artur <artur@iqu.com>
2015-01-28 14:55:54 +0000

    adding logger so inject into Resources classes

--- src/Resources.php
+++ src/Resources.php
@@ -8,15 +8,21 @@
 
 namespace slc\MVC;
 
+use slc\MVC\Logger;
+
 class Resources {
 	private static $ResourceInstances = array();
-	public static function Factory($Type, $Id) {
-		if(!isset(static::$ResourceInstances[$Type][$Id])) {
-			Base::Factory()->importFile(array('Resources::'.$Type));
-			$cls = 'slc\MVC\Resources\\'.$Type;
-			static::$ResourceInstances[$Type][$Id] = new $cls($Id);
-		}
-		return static::$ResourceInstances[$Type][$Id];
+	public static function Factory($Type, $Id, Logger $logger = null)
+    {
+        if (!isset(static::$ResourceInstances[$Type][$Id])) {
+            Base::Factory()->importFile(array('Resources::'.$Type));
+			$cls = 'slc\MVC\Resources\\' . $Type;
+            static::$ResourceInstances[$Type][$Id] = new $cls($Id);
+        }
+        if (method_exists(static::$ResourceInstances[$Type][$Id], 'setLogger')) {
+            static::$ResourceInstances[$Type][$Id]->setLogger($logger);
+        }
+        return static::$ResourceInstances[$Type][$Id];
 	}
 }
 
